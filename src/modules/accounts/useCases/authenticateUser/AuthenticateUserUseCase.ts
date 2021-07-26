caeb1c8da01def5b509edf735f4e8563e4c99aba
Recieire Carbelim <recieiremarcos@gmail.com>
2021-07-26 00:32:27 -0300

    custom error

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -27,13 +28,13 @@ class AuthenticateUserUseCase {
         const user = await this.usersRepository.findByEmail(email);
 
         if (!user) {
-            throw new Error("E-mail or password incorrect!");
+            throw new AppError("E-mail or password incorrect!");
         }
 
         const passwordMatch = await compare(password, user.password);
 
         if (!passwordMatch) {
-            throw new Error("E-mail or password incorrect!");
+            throw new AppError("E-mail or password incorrect!");
         }
 
         const token = sign({}, "759b0af65df72c4941f2c05a5427c121", {
